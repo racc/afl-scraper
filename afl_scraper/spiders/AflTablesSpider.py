1083a7651299dbc26bfaae4085c79bd363675b5f
jason <jason@raynor>
2016-09-14 19:00:43 +0930

    Got scraper working. Added throttling

--- afl_scraper/spiders/AflTablesSpider.py
+++ afl_scraper/spiders/AflTablesSpider.py
@@ -1,9 +1,11 @@
 import scrapy
+import os
 
 class AflTablesSpider(scrapy.Spider):
     name = "afl_tables"
     allowed_domains = ["afltables.com"]
     start_urls = map(lambda x: "http://afltables.com/afl/seas/" + str(x) + ".html", range(1997, 2016))
+    download_delay = 10
 
     def parse(self, response):
         for href in response.xpath('//a[contains(@href, "/stats/games")]/@href'):
@@ -11,8 +13,17 @@ class AflTablesSpider(scrapy.Spider):
             yield scrapy.Request(url, callback=self.parse_game_stats)
 
     def parse_game_stats(self, response):
-        round = str(response.xpath("//table[1]/tr[1]/td[2]/text()").extract()[0]).strip()
-        season = str(response.xpath('//a[contains(@href, "/seas")]/text()').extract()[0])
+        season = str(response.xpath('//a[contains(@href, "/seas")]/text()').extract()[0]).replace("Games", "").strip()
+        round_num = str(response.xpath("//table[1]/tr[1]/td[2]/text()").extract()[0]).strip()
+        team_one = str(response.xpath("//table[1]/tr[2]/td[1]/a/text()").extract()[0])
+        team_two = str(response.xpath("//table[1]/tr[3]/td[1]/a/text()").extract()[0])
 
+        filename = "data/" + season + "/" + round_num + "/" + team_one + "_vs_" + team_two + ".html"
+        dirname = os.path.dirname(filename)
+        if not os.path.exists(dirname):
+            os.makedirs(dirname)
 
-        return ""
+        with open (filename, 'wb') as f:
+            f.write(response.body)
+
+        return None
